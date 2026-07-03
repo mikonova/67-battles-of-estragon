fd3c97fd886bbcc145995ae7589066b0a84464c2
mikonova <mikk4business@gmail.com>
2026-07-03 13:23:55 +0300

    [FIX] resolved conflicts
    [CHORE] removed conflicts

--- scripts/camera2d.cs
+++ scripts/camera2d.cs
@@ -6,12 +6,14 @@ public partial class camera2d : Camera2D
 	private Node2D _target;
 	private Vector2 _cameraPos;
 	private float _cameraPosY;
+	private float _defaultCameraX;
 	private float _deadZone = 100.0f;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_target = GetParent<Node2D>();
 		_cameraPos = _target.GlobalPosition;
+		_defaultCameraX = _target.GlobalPosition.X;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,8 +21,8 @@ public partial class camera2d : Camera2D
 	{
 		if (Mathf.Abs(_target.GlobalPosition.Y - _cameraPosY) > _deadZone)
 		{
-			_cameraPosY = _target.GlobalPosition.Y;
+			_cameraPosY = _target.GlobalPosition.Y - _deadZone;
 		}
-		GlobalPosition = new Vector2(GlobalPosition.X, _cameraPosY);
+		GlobalPosition = new Vector2(_defaultCameraX, _cameraPosY);
 	}
 }
